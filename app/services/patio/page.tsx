055c646d19df6b5eec868a99a1ba852717a03aeb
Auvy <fahimur@gmail.com>
2026-02-03 22:32:55 +0600

    service components moved to the patio folder

--- app/services/patio/page.tsx
+++ app/services/patio/page.tsx
@@ -1,10 +1,10 @@
-import HeroSection from "../components/HeroSection";
-import PageContent from "../components/PageContent";
-import WhyProperty from "../components/WhyProperty";
-import Capabilities from "../components/Capabilities";
-import Includes from "../components/Includes";
-import ProjectScenarios from "../components/ProjectScenarios";
-import FAQSection from "../components/FAQSection";
+import HeroSection from "./components/HeroSection";
+import PageContent from "./components/PageContent";
+import WhyProperty from "./components/WhyProperty";
+import Capabilities from "./components/Capabilities";
+import Includes from "./components/Includes";
+import ProjectScenarios from "./components/ProjectScenarios";
+import FAQSection from "./components/FAQSection";
 
 const page = () => {
   return (
