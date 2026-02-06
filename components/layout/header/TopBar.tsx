cf591c71ebe79f7c1c65dea51d6ac8c4a230324b
Auvy <fahimur@gmail.com>
2026-02-06 07:34:48 +0600

    ui (responsive-navBar): topbar

--- components/layout/header/TopBar.tsx
+++ components/layout/header/TopBar.tsx
@@ -1,18 +1,9 @@
-import Image from "next/image";
-
 const TopBar = () => {
   return (
-    <div className="base-padding bg-brand-teal font-medium text-sm text-white h-10 flex items-center justify-between">
+    <div className="base-padding bg-brand-teal font-medium text-sm text-white h-8 flex items-center justify-between">
       <p className="hidden md:block">Commercial & Residential Concrete</p>
       <div className="flex items-center cursor-pointer ml-auto">
-        <Image
-          src="/icons/phone-icon.svg"
-          alt="phone"
-          width={16}
-          height={16}
-          className="mr-2"
-        />
-        <p>Call (323) 555-0123</p>
+        <p>Call 323-555-0123</p>
       </div>
     </div>
   );
