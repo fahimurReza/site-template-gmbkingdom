924b0f0efdec81861e6fdbfe40c893f2c0ef2955
Auvy <fahimur@gmail.com>
2026-02-07 23:03:55 +0600

    (ui): responsive-footer-05

--- components/layout/footer/CopyRight.tsx
+++ components/layout/footer/CopyRight.tsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const CopyRight = () => {
   return (
-    <main className="base-padding bg-white/5 border-t border-brand-gray py-3">
-      <h4 className="text-white/70 uppercase text-[10px] font-medium tracking-[2px]">
+    <main className="base-padding bg-white/5 border-t border-brand-gray py-6 text-center">
+      <h4 className="text-white/80 uppercase text-[10px] font-medium tracking-[2px]">
         © 2025 Frisco Concrete Contractors. All rights reserved.
       </h4>
-      <p className="text-white/50 mt-1 text-[10px] tracking-[1px]">
+      <p className="text-white/60 mt-1.5 text-[10px] tracking-[1px]">
         The North Texas leader in residential and commercial concrete
         contracting.
       </p>
