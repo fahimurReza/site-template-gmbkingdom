9241ec78784c0640ba72d5b16349b012c6dd282e
Auvy <fahimur@gmail.com>
2026-02-07 23:08:07 +0600

    (ui): responsive-footer-06

--- components/layout/footer/ContactInfo.tsx
+++ components/layout/footer/ContactInfo.tsx
@@ -9,8 +9,8 @@ const ContactInfo = () => {
         href="tel:+13332343454"
         className="mt-6 flex gap-6 items-center cursor-pointer"
       >
-        <div className="bg-white/10 p-2 rounded">
-          <PhoneIcon className="w-6 aspect-square" />
+        <div className="bg-white/15 p-2 rounded">
+          <PhoneIcon className="w-5 aspect-square" />
         </div>
         <div className="items-center content-center">
           <p className="text-[20px] font-medium">333-234-3454</p>
@@ -18,8 +18,8 @@ const ContactInfo = () => {
       </a>
 
       <a className="mt-6 flex gap-6 items-center cursor-pointer">
-        <div className="bg-white/10 p-2 rounded">
-          <ClockIcon className="w-6 aspect-square" stroke="10" />
+        <div className="bg-white/15 p-2 rounded">
+          <ClockIcon className="w-5 aspect-square" stroke="10" />
         </div>
         <div className="items-center content-center">
           <p className="text-[16px] font-medium">
@@ -30,8 +30,8 @@ const ContactInfo = () => {
       </a>
 
       <a className="mt-6 flex gap-6 items-center cursor-pointer">
-        <div className="bg-white/10 p-2 rounded">
-          <LocationIcon className="w-6 aspect-square" />
+        <div className="bg-white/15 p-2 rounded">
+          <LocationIcon className="w-5 aspect-square" />
         </div>
 
         <div className="items-center content-center">
