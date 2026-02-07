e69ae2af8a14a6a4b6ede86eea54293b6d9b6cde
Auvy <fahimur@gmail.com>
2026-02-08 05:46:35 +0600

    (ui): top-bar-gradient

--- components/layout/header/TopBar.tsx
+++ components/layout/header/TopBar.tsx
@@ -1,6 +1,6 @@
 const TopBar = () => {
   return (
-    <div className="base-padding bg-brand-teal font-medium text-sm text-white h-8 flex items-center justify-between">
+    <div className="base-padding bg-linear-to-r from-teal-950 to-teal-800 font-medium text-sm text-white h-8 flex items-center justify-between">
       <p className="hidden md:block">Commercial & Residential Concrete</p>
       <div className="flex items-center cursor-pointer ml-auto">
         <p>Call 323-555-0123</p>
