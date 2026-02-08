c43a857f3ee461abb157322ea130d8c7bcfa73d3
Auvy <fahimur@gmail.com>
2026-02-08 06:05:45 +0600

    (ui): responsive-about-us-01

--- app/about/components/HeroSection.tsx
+++ app/about/components/HeroSection.tsx
@@ -1,7 +1,10 @@
 const HeroSection = () => {
   return (
-    <section className="base-padding bg-linear-to-r from-brand-teal to-brand-teal-light text-white">
-      <div className="w-full py-18 flex flex-col items-center gap-y-5">
+    <section>
+      <div className="absolute inset-0 bg-linear-to-r from-teal-950 to-teal-700 -z-10" />
+      <div className="absolute inset-0 bg-linear-to-r from-black/40 to-transparent -z-10" />
+
+      <div className="base-padding text-white w-full py-12 flex flex-col items-center gap-y-6">
         <div
           className={`flex items-center gap-4 border border-white/20 rounded-full
          bg-white/10 px-6 py-1 text-xs tracking-[3px] font-semibold w-fit uppercase`}
@@ -12,7 +15,7 @@ const HeroSection = () => {
           </span>
           About Us
         </div>
-        <h2 className="text-center text-[48px] font-semibold">
+        <h2 className="text-center text-[42px] font-semibold leading-12">
           Built on Quality, Delivered on Schedule
         </h2>
         <p className="text-lg text-center max-w-4xl">
